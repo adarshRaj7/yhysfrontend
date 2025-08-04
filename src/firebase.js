b172d073b4247474201b7e782a653eca32c8191c
Adarsh Raj <adarshrj62@gmail.com>
2025-08-04 21:25:28 +0530

    Security fix: Move Firebase config to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAaY7DlAq_pDAHd0cO59CaH74zBt-m7kNo",
-  authDomain: "yhys-7f303.firebaseapp.com",
-  projectId: "yhys-7f303",
-  storageBucket: "yhys-7f303.firebasestorage.app",
-  messagingSenderId: "681655520816",
-  appId: "1:681655520816:web:9d9bf1875e4cfd277167f5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
